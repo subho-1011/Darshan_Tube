7caeb72aae216ad6fa4c81cf1fb39d3f3d68f5ef
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2024-10-29 17:40:26 +0530

    #

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -15,7 +15,9 @@ export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export const useAppStore: () => AppStore = useStore;
 
-const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+const API_URL =
+    process.env.NEXT_PUBLIC_BACKEND_URL ||
+    "https://darhan-tube-backend.vercel.app";
 
 export const api = axios.create({
     baseURL: `${API_URL}/api/v1/`,
